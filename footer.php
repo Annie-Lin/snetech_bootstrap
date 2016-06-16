f7a0169fe31dd01dfc049547ce63363928101fc7
Annie-Lin <sheep7628@gmail.com>
2016-06-16 10:53:36 +0800

    [ADD] index grid system

--- footer.php
+++ footer.php
@@ -7,19 +7,22 @@
 ?>
 
 			</div><!--.site-content-->
+			<!-- Footer -->
 
-			<!-- Footer --> 
-		    <h6>版權所有© 2016 <?php bloginfo ( 'name' ) ; ?></h6>
-		    <p>Powered By <a rel="external" title="WordPress主頁" class="link" href="http://wordpress.org/"></p>
-
-		    <p>service_sne@snetech.com.tw</p>
-		    <p>TEL:04-2539-3970</p>
-		    <p>FAX:04-2539-3970</p>
-		    <p>427台中市潭子區祥和路47號7樓</p>
-			
+			<div class="row">
+				<div class="col-xs-12 col-sm-3">
+					<h1><?php bloginfo ( 'name' ) ; ?></h1>
+				</div>
+				<div class="col-xs-12 col-sm-9">
+				    <p>service_sne@snetech.com.tw</p>
+				    <p>TEL:04-2539-3970</p>
+				    <p>FAX:04-2539-3970</p>
+				    <p>427台中市潭子區祥和路47號7樓</p>
+				    <p>Powered By <a rel="external" title="WordPress主頁" class="link" href="http://wordpress.org/"></p>
+					<p>版權所有© 2016 <?php bloginfo ( 'name' ) ; ?></p>
+				</div>
+			</div>
 		</div><!--.container page-container-->
-		
-		
 		<!--wordpress footer-->
 		<?php wp_footer(); ?> 
 	</body>
--- index.php
+++ index.php
@@ -15,22 +15,22 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 
 <div id="index_main_container">
 
-	<div id="intro" class="index_section">
+	<div id="index_intro" class="index_section row">
         <h1>資產異動管理可以更加優化</h1>
         <h6>力十幫您找出最佳解決方案</h6>
-        <!-- Standard button -->
-<button type="button" class="btn btn-primary">Default</button>
     </div>
 
-    <div id="solution" class="index_section">
-        <h2>專業的 RFID 系統整合服務</br>讓管理變得更容易</h2>
-        <img alt="" src="<?php bloginfo('template_url'); ?>/img/610x150.gif" />
+    <div id="index_solution" class="index_section row">
+        <h2 class="col-xs-12 col-sm-12">專業的 RFID 系統整合服務</br>讓管理變得更容易</h2>
+        <div class="col-xs-12 col-sm-12">
+        	<img alt="" src="<?php bloginfo('template_url'); ?>/img/610x150.gif" />
+    	</div>
 
         <!-- category:solution && tag:index -->
         <?php if ( have_posts ( ) ) : while ( have_posts ( ) ) : the_post ( ) ; ?>
             <?php if (in_category("solution") && has_tag("index")): ?>
             <!-- 單篇post骨架 -->
-            <div class="index_solution_item">
+            <div class="index_solution_item col-xs-12 col-sm-15">
                 <img alt="" src="<?php bloginfo('template_url'); ?>/img/Art_Artdesigner.lv.png" />
                 <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
                 <p><?php the_content(); ?></p>
@@ -43,29 +43,31 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
         <?php endif ; ?>
     </div>
 
-    <div id="customization" class="index_section">
-        <h2>針對您的需求</br>規劃最適合的方案</h2>
-        <img alt="" src="<?php bloginfo('template_url'); ?>/img/610x150.gif" />
-        <div>
-            <h3>成功案例</h3>
-            <!-- category:case && tag:index -->
-            <?php if ( have_posts ( ) ) : while ( have_posts ( ) ) : the_post ( ) ; ?>
-                <?php if (in_category("case") && has_tag("index")): ?>
-                <!-- 單篇post骨架 -->
-                <div class="post">
-                    <!-- Post Title -->
-                    <h6 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
-                    <img alt="" src="<?php bloginfo('template_url'); ?>/img/223x112.gif" />
-                </div>
-                <?php endif ; ?>
-            <?php endwhile ; ?> <?php else : ?>     
-                <!-- 輸出找不到文章提示 -->
-                <h1>沒有成功案例</h1>
-            <?php endif ; ?>
+    <div id="index_customization" class="index_section row">
+        <h2 class="col-xs-12 col-sm-12">針對您的需求</br>規劃最適合的方案</h2>
+        <div class="col-xs-12 col-sm-12">
+        	<img alt="" src="<?php bloginfo('template_url'); ?>/img/610x150.gif" />
+    	</div>
+    	<div class="index_case col-xs-12 col-sm-12">
+	        <h3 class="col-xs-12 col-sm-12">成功案例</h3>
+	        <!-- category:case && tag:index -->
+	        <?php if ( have_posts ( ) ) : while ( have_posts ( ) ) : the_post ( ) ; ?>
+	            <?php if (in_category("case") && has_tag("index")): ?>
+	            <!-- 單篇post骨架 -->
+	            <div class="index_case_item col-xs-12 col-sm-3">
+	                <!-- Post Title -->
+	                <h6 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+	                <img alt="" src="<?php bloginfo('template_url'); ?>/img/223x112.gif" />
+	            </div>
+	            <?php endif ; ?>
+	        <?php endwhile ; ?> <?php else : ?>     
+	            <!-- 輸出找不到文章提示 -->
+	            <h1>沒有成功案例</h1>
+	        <?php endif ; ?>
         </div>
     </div>
 
-    <div id="team" class="index_section">
+    <div id="index_team" class="index_section row">
         <h2>堅強的團隊</br>提供最專業的服務</h2>
         <p>說說為什麼說說為什麼說說為什麼說說為什麼說說為什麼說說為什麼說說為什麼</br>說說為什麼說說為什麼說說為什麼說說為什麼說說為什麼說說為什麼說說為什麼說說為什麼說說為什麼</p>
         <img alt="" src="<?php bloginfo('template_url'); ?>/img/610x150.gif" /></br>
@@ -73,5 +75,4 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
     </div>
 </div>	
 
-<?php get_sidebar('right'); ?> 
 <?php get_footer(); ?> 
